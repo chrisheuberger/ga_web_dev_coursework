a302cbd2b3f59013de40b4c9385f6c6cd84d94f1
Chris Heuberger <christopherheuberger@gmail.com>
2015-03-06 15:35:52 -0500

    Update lotr.js

--- w07/d03/lotr_dom_manipulation/lotr.js
+++ w07/d03/lotr_dom_manipulation/lotr.js
@@ -22,117 +22,192 @@ var baddies = [
 
 var lands = ["The Shire", "Rivendell", "Mordor"];
 
-// 1
-function makeMiddleEarth(lands) {
-    // create a section tag with an id of middle-earth
-    var section = $('<section>', {id: 'middle-earth'});
-    $(section).appendTo('body');
-    // add each land as an article tag
-    var manyLands = function(){
-      $.each(lands, function(index, value) {
-        var land = $('<article>');
-        var showLand = $(section).append(land);
-        $(land).append($('<h1>').html(value));
-      });
-    };
-    // inside each article tag include an h1 with the name of the land
-    $('article').html('<h1>' + manyLands() + '</h1>');
+// 1 //
+
+function makeMiddleEarth(lands){
+  var landSection = $('<section>').addClass("middle-earth");
+  for (var i = 0; i < lands.length; i++){
+    var emptyArticle = $('<article>');
+    var emptyHone = $('<h1>');
+    emptyHone.html(lands[i]);
+    emptyArticle.append(emptyHone);
+    landSection.append(emptyArticle);
+  }
+  $('body').append(landSection);
 }
 
-$(document).ready(function() {
-  makeMiddleEarth(lands);
-});
+makeMiddleEarth(lands);
 
-// 2
-function makeHobbits(hobbits) {
-  // display an unordered list of hobbits in the shire
-  var list = $('article').eq(0).after('<ul>');
-  var manyHobbits = function(){
-    $.each(hobbits, function(index, value){
-      var hobbit = $('<li>');
-      var showHobbit = $(list).append(hobbit);
-      $(hobbit).html(value);
-      $(hobbit).addClass('hobbit');
-      // $('<li>', {text: value}).appendTo(list);
-      // $('li').addClass('hobbit');
-    });
-  };
-  // give each hobbit a class of hobbit
-};
+// 2 //
 
-$(document).ready(function() {
-  makeHobbits(hobbits);
-});
-
-// 3
-function keepItSecretKeepItSafe() {
-  // create a div with an id of 'the-ring'
-  // add the ring as a child of Frodo
+function makeHobbits(hobbits){
+  var newUl = $('<ul>');
+  for (var i = 0; i < hobbits.length; i++){
+    var emptyLi = $('<li>');
+    var hobbitLi = emptyLi.html(hobbits[i]).addClass('hobbit');
+    newUl.append(hobbitLi);
+  }
+  $('body').append(newUl);
 }
 
-// 4
-function makeBaddies(baddies) {
-  // display an unordered list of baddies in Mordor
+makeHobbits(hobbits);
+
+// 3 //
+
+function keepItSecretKeepItSafe(){
+  var ringDiv = $('<div>').addClass('the-ring');
+  var frodo = $('li:contains(Frodo Baggins)');
+  frodo.append(ringDiv);
 }
 
-// 5
-function makeBuddies(buddies) {
-  // create an aside tag
-  // display an unordered list of buddies in the aside
-  // Make the Gandalf text node have a grey background
+keepItSecretKeepItSafe();
+
+// 4 //
+
+function makeBaddies(baddies){
+  var emptyUl = $('<ul>');
+  for (var i = 0; i < baddies.length; i++){
+    var emptyLi = $('<li>');
+    var baddieLi = emptyLi.html(baddies[i]);
+    emptyUl.append(baddieLi);
+  }
+  $('body').append(emptyUl);
 }
 
-// 6
-function leaveTheShire() {
-  // assemble the hobbits and move them to Rivendell
+makeBaddies(baddies);
+
+// 5 //
+
+function makeBuddies(buddies){
+  var newAside = $('<aside>');
+  var newUl = $('<ul>');
+  for (var i = 0; i < buddies.length; i++){
+    var newLi = $('<li>');
+    newLi.html(buddies[i]);
+    newUl.append(newLi);
+  }
+  newAside.append(newUl);
+  $('body').append(newAside);
+  var gandalf = $('li:contains(Gandalf the Grey)');
+  gandalf.css('backgroundColor', 'lightgrey');
 }
 
-// 7
-function beautifulStranger() {
-  // change the buddy 'Strider' textnode to "Aragorn"
+makeBuddies(buddies);
+
+// 6 //
+
+function leaveTheShire(){
+  var rivendellArticle = $('article:contains(Rivendell)');
+  var oldUl = $('ul:contains(Frodo Baggins)');
+  var newUl = $('<ul>');
+  var hobbitClan = $('.hobbit');
+  for (var i = 0; i < hobbitClan.length; i++){
+    newUl.append(hobbitClan[i]);
+  }
+  oldUl.remove();
+  rivendellArticle.append(newUl);
 }
 
-// 8
-function forgeTheFellowShip() {
-  // move the hobbits and the buddies to Rivendell
-  // create a new div called 'the-fellowship'
-  // add each hobbit and buddy one at a time to 'the-fellowship'
-  // after each character is added make an alert that they have joined your party
+leaveTheShire();
+
+// 7 //
+
+function BeautifulStranger(){
+  $('li:contains(Strider)').html('Aragorn');
 }
 
-// 9
-function theBalrog() {
-  // change the 'Gandalf' textNode to 'Gandalf the White'
-  // apply style to the element
-  // make the background 'white', add a grey border
+BeautifulStranger();
+
+// 8 //
+
+function forgeTheFellowship(){
+  var rivendellArticle = $('article:contains(Rivendell)');
+  var hobbitClan = $('ul:contains(Frodo Baggins)');
+  var buddieClan = $('ul:contains(Gandalf the Grey)');
+  var combinedClan = $.merge(hobbitClan.children(), buddieClan.children());
+  $('aside').remove();
+  var fellowshipDiv = $('<div>').addClass('the-fellowship');
+  var fellowshipUl = $('<ul>');
+  for (var i = 0; i < combinedClan.length; i++){
+    fellowshipUl.append(combinedClan[i]);
+  }
+  hobbitClan.remove();
+  fellowshipDiv.append(fellowshipUl);
+  rivendellArticle.append(fellowshipDiv);
+  alert('The Fellowship has been formed in Rivendell.');
 }
 
-// 10
-function hornOfGondor() {
-  // pop up an alert that the horn of gondor has been blown
-  // Boromir's been killed by the Uruk-hai!
-  // put a linethrough on boromir's name
-  // Remove the Uruk-Hai from the Baddies on the page
-  // Remove Boromir from the Fellowship
-  // Put Boromir in the Footer
+// 9 //
+
+forgeTheFellowship();
+
+function theBalrog(){
+  var gandalf = $('li:contains(Gandalf the Grey)');
+  gandalf.html('Gandalf the White');
+  gandalf.css('backgroundColor', 'White');
+  gandalf.css('border', '1px solid grey');
 }
 
-//11
+theBalrog();
+
+// 10 //
+
+function hornOfGondor(){
+  alert("Oh shit, the Horn of Gondor has been blown!");
+  alert("...and Boromir was killed by the Uruk-hai!");
+  var fellowship = $('.the-fellowship');
+  var boromir = $('li:contains(Boromir)');
+  boromir.css('textDecoration', 'line-through');
+  var urukHai = $('li:contains(The Uruk-hai)');
+  urukHai.remove();
+  var footer = $('<footer>');
+  footer.append(boromir);
+  $('body').append(footer);
+}
+
+hornOfGondor();
+
+// 11 //
+
 function itsDangerousToGoAlone(){
-  // take Frodo and Sam out of the fellowship and move them to Mordor
-  // add a div with an id of 'mount-doom' to Mordor
+  var frodo = $('li:contains(Frodo Baggins)');
+  var sam = $('li:contains(Samwise)');
+  var emptyUl = $('<ul>');
+  var mordor = $('article:contains(Mordor)');
+  emptyUl.append(sam);
+  emptyUl.append(frodo);
+  mordor.append(emptyUl);
+  var mtDoom = $('<div>').addClass('mount-doom');
+  mordor.append(mtDoom);
 }
 
-// 12
-function weWantsIt() {
-  // Create a div with an id of 'gollum' and add it to Mordor
-  // Remove the ring from Frodo and give it to Gollum
-  // Move Gollum into Mount Doom
+itsDangerousToGoAlone();
+
+// 12 //
+
+function weWantsIt(){
+  var gollumDiv = $('<div>');
+  gollumDiv.addClass('gollum');
+  var mordor = $('article:contains(Mordor)');
+  var ring = $('.the-ring');
+  mordor.append(gollumDiv);
+  gollumDiv.append(ring);
 }
 
-// 13
-function thereAndBackAgain() {
-  // remove Gollum and the Ring from the document
-  // remove all the baddies from the document
-  // Move all the hobbits back to the shire
+weWantsIt();
+
+// 13 //
+
+function thereAndBackAgain(){
+  var gollum = $('.gollum');
+  var ring = $('.the-ring');
+  gollum.remove();
+  ring.remove();
+  var allHobbits = $('.hobbit');
+  var theShire = $('article:contains(The Shire)');
+  for (var i = 0; i < allHobbits.length; i++){
+    theShire.append(allHobbits[i]);
+  }
 }
+
+thereAndBackAgain();
